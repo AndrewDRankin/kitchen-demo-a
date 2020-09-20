c9c525a9470844b7455905dcf0684854187fa8ae
DevonDerbin <wave@andrewdrankin.com>
2020-09-20 22:38:50 +0100

    Tidy code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+// Header change
 let header = document.getElementById('header');
 
 window.addEventListener('scroll', () => {
     window.pageYOffset > 150 ? header.classList.add('header--opaque') : header.classList.remove('header--opaque')
+});
+
+// Menu toggle
+let menuButton = document.getElementById('menu-button');
+let menuOpen = false;
+
+menuButton.addEventListener('click', () => {
+    menuOpen = !menuOpen;
+    menuOpen === true ? header.classList.add('menu-open') : header.classList.remove('menu-open');
 });
